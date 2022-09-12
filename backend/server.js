baf5828f2be958fdb30a45ed95e0e12f9186bcb6
madhushreesk <kulkarnimadhushree785@gmail.com>
2022-09-12 02:53:33 -0700

    api/user api/user/login working

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+const { authUser } = require('./controllers/userController')
+
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());    // to accept JSON data
+
+
+app.get("/", (req, res) => {
+    res.send("API is running successfully!");
+});
+
+app.use('/api/user', userRoutes)
+app.use('/api/user/login', authUser)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold))
